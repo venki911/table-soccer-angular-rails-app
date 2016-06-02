5865f611e9b092c20111c4416acedf7045b0bcb4
Igor Kobka <trobule1@gmail.com>
2016-06-02 10:38:58 +0300

    fix faker locale on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  Faker::Config.locale = :ru
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
